e79d130dd90d69e2052ed690617f958a1dd890ee
iodex99 <dwahnilbaria19@gmail.com>
2026-01-02 12:11:25 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "OnlyAds",
+  description: "You are merely data"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
